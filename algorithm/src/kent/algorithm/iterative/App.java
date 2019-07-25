ab53c4815dbf82f384374b6b07cc86a0fee6a7f3
KentFan <cjfan2005@gmail.com>
2019-07-25 09:42:32 +0800

    update the value.

--- algorithm/src/kent/algorithm/iterative/App.java
+++ algorithm/src/kent/algorithm/iterative/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Algorithm a = new Algorithm();
+		IterativeAndRecursive a = new IterativeAndRecursive();
 		System.out.println("Iterative result = " + a.sumIterative(3));
 		
 		System.out.println("Recursive result = " + a.sumRecursive(5));
